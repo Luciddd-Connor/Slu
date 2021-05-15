4056e697c682b38f534fb47197800e8ea28e3039
Luciddd-Connor <84215044+Luciddd-Connor@users.noreply.github.com>
2021-05-14 23:16:09 -0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Can be multiple prefixes, like this: ("!", "?")
-BOT_PREFIX = ("$")
-TOKEN = "ODQyODE1NDc2MDIyMTE2Mzcy.YJ6zFA.qRaFA71B4iEma4A_mqDg-4wwQzA"
-APPLICATION_ID = "842815476022116372"
-BLACKLIST = []
-STARTUP_COGS = [
-    "cogs.general", "cogs.help", "cogs.moderation", "cogs.owner",
-]
